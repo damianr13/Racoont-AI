fb0ff14a24c52cdc408b35a144f2c4a42b78d9ae
damianr13 <robert-andrei.damian@panprices.com>
2023-01-31 09:44:43 +0100

    Put everything together into one CLI script

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import os
 import uuid
 
+import cohere
 import librosa
 import soundfile as sf
 from TTS.utils.manage import ModelManager
 from TTS.utils.synthesizer import Synthesizer
+import pickle5 as pickle
+from pydantic import BaseSettings, Field
+import numpy as np
+
+
+class Settings(BaseSettings):
+    cohere_api_key: str = Field()
+
+    class Config:
+        env_file = ".env"
+
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
 
 def convert_audio(
@@ -47,9 +62,55 @@ def tts_with_voice(text: str, voice_path: str):
     synthesizer.save_wav(wav, f"output/{voice_path.split('/')[-1].split('.')[0]}.wav")
 
 
+def generate_story(morale, main_character):
+    with open("resources/fables-with-emb.pkl", "rb") as f:
+        resource = pickle.load(f)
+
+    fables = resource["fables"]
+    fables_emb = resource["embs"]
+
+    settings = Settings()
+    co = cohere.Client(settings.cohere_api_key)
+    morale_embed = co.embed([morale]).embeddings[0]
+
+    distances = [
+        cosine_similarity(np.array(e), np.array(morale_embed)) for e in fables_emb
+    ]
+    closest_fable = fables[np.argmax(distances)]
+
+    prompt = f"""
+        This is a short fable. 
+        "{closest_fable}" 
+        
+        Based on it, create a longer story that expands over multiple paragraphs. Each character should have a name \
+        and the final story should include more characters, but keep the same plot line. Use simple language, as this \
+        should be a children bed time story.
+        Name of the main character: {main_character}
+        
+        Once upon a time
+    """
+    generate_response = co.generate(
+        model="command-xlarge-20221108",
+        prompt=prompt,
+        max_tokens=1400,
+        temperature=1.9,
+        k=450,
+        p=0.9,
+        frequency_penalty=0.25,
+        presence_penalty=0.2,
+        stop_sequences=[],
+        return_likelihoods="NONE",
+    )
+    return generate_response.generations[0].text
+
+
 def main():
     # Ask for file path
     audio_file = input("Enter file path: ")
+
+    # Ask for a morale input
+    input_morale = input("Enter a morale: ")
+    input_main_character = input("Give the name of the main character: ")
 
     # save with uuid
     output_file = f"voices/{uuid.uuid4()}.wav"
@@ -57,8 +118,11 @@ def main():
     # Convert audio file
     convert_audio(audio_file, output_file)
 
+    # Generate story
+    story = generate_story(input_morale, input_main_character)
+
     # TTS with voice
-    tts_with_voice("Hello world", output_file)
+    tts_with_voice(story, output_file)
 
 
 if __name__ == "__main__":
